7aba41cbba299853125bf3637e1b402922f354f6
Lin-Zhiyi <100063128+Lin-Zhiyi@users.noreply.github.com>
2026-08-01 12:10:20 +0800

    Change SameSite attribute of Set-Cookie header

--- functions/api/auth/logout.js
+++ functions/api/auth/logout.js
@@ -2,7 +2,7 @@
 export async function onRequestPost() {
   const headers = new Headers({
     'Content-Type': 'application/json',
-    'Set-Cookie': 'token=; HttpOnly; Secure; SameSite=Strict; Path=/; Max-Age=0'
+    'Set-Cookie': 'token=; HttpOnly; Secure; SameSite=Lax; Path=/; Max-Age=0'
   });
   return new Response(JSON.stringify({ success: true }), { headers });
 }
